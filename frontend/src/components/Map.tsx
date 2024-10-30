776c0e1d635fa43b01eff961e0473442ce0c2947
Cecilia Rissanen <cecilia.rissanen@iths.com>
2024-10-30 14:11:47 +0100

    without map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,19 +1,19 @@
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+// import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 export const MapComponent = () => {
-  const latitude = 57.74806503952224;
-  const longitude = 12.175686427624894;
+  // const latitude = 57.74806503952224;
+  // const longitude = 12.175686427624894;
 
-  const location = { lat: latitude, lng: longitude };
+  // const location = { lat: latitude, lng: longitude };
 
   return (
     <>
       <div className="map-container">
-        <APIProvider apiKey="AIzaSyDjatqSt3e0iBuNPO0NenLg10guP0z4Ch4">
+        {/* <APIProvider apiKey="AIzaSyDjatqSt3e0iBuNPO0NenLg10guP0z4Ch4">
           <Map defaultZoom={14} defaultCenter={location}>
             <Marker position={location} />
           </Map>
-        </APIProvider>
+        </APIProvider> */}
       </div>
     </>
   );
