2419f4ffc59325a01bbcba62d4feed7fb32a2f3b
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2025-01-12 14:39:08 +0100

    refactor: remove fragment

--- frontend/src/Views/WeddingPage.tsx
+++ frontend/src/Views/WeddingPage.tsx
@@ -8,38 +8,36 @@ import { HotelMarkup } from "../assets/Texts";
 import { MapComponent } from "../components/Map";
 export const WeddingPage = () => {
   return (
-    <>
-      <div
-        className="flex flex-col w-full sm:text-center sm:justify-center font-alumni font-light text-darkblue text-xl p-4"
-        id="wedding"
-      >
-        <h1 className="font-windsong text-5xl sm:ml-8 text-center mb-8">
-          Bröllopet
-        </h1>
-        <Image imageSource={bild2} altText="couple by the lake" />
-        <div id="osa" className="mb-8">
-          <OSAMarkup />
-          <OSAPage />
-        </div>
-        <div id="hotel" className="flex flex-col mb-8">
-          <h2 className="font-windsong text-3xl mb-2">Hotell</h2>
-          <div className="flex flex-col w-full">
-            <HotelMarkup />
-            <Image imageSource={bild3} altText="hotel" />
-          </div>
-        </div>
-        <div id="directions" className="mb-8">
-          <h2 className="font-windsong text-3xl mb-2 ">Hitta hit</h2>
-
-          <div className="flex flex-col font-alumni">
-            <DirectionsMarkup />
-            <MapComponent />
-          </div>
-        </div>
-        <div className="flex flex-col mb-8" id="gifts">
-          <GiftMarkup />
+    <div
+      className="flex flex-col w-full sm:text-center sm:justify-center font-alumni font-light text-darkblue text-xl p-4"
+      id="wedding"
+    >
+      <h1 className="font-windsong text-5xl sm:ml-8 text-center mb-8">
+        Bröllopet
+      </h1>
+      <Image imageSource={bild2} altText="couple by the lake" />
+      <div id="osa" className="mb-8">
+        <OSAMarkup />
+        <OSAPage />
+      </div>
+      <div id="hotel" className="flex flex-col mb-8">
+        <h2 className="font-windsong text-3xl mb-2">Hotell</h2>
+        <div className="flex flex-col w-full">
+          <HotelMarkup />
+          <Image imageSource={bild3} altText="hotel" />
         </div>
       </div>
-    </>
+      <div id="directions" className="mb-8">
+        <h2 className="font-windsong text-3xl mb-2 ">Hitta hit</h2>
+
+        <div className="flex flex-col font-alumni">
+          <DirectionsMarkup />
+          <MapComponent />
+        </div>
+      </div>
+      <div className="flex flex-col mb-8" id="gifts">
+        <GiftMarkup />
+      </div>
+    </div>
   );
 };
